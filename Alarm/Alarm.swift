bf49e584d2f8b22bc547f171b6fedaf76108ef3c
Pierre Toroyan <p.toroyan@gmail.com>
2017-02-16 00:01:12 -0800

    NSCoding
    - added NSCoding for saving feature

--- Alarm/Alarm.swift
+++ Alarm/Alarm.swift
@@ -53,24 +53,52 @@ class Alarm: NSObject, NSCoding {
     }
     
     
-    required init?(coder aDecoder: NSCoder) {
-    	guard let name = aDecoder.decodeObject(forKey: kName) as? String,
-        	let fireTimeFromMidnight = aDecoder.decodeObject(forKey: kFireTimeFromMidnight) as? TimeInterval,
-        	let enabled = aDecoder.decodeObject(forKey: kEnabled) as? Bool,
-            let uuid = aDecoder.decodeObject(forKey: kUUID) as? String else {return nil}
-        
-        self.name = name
-        self.fireTimeFromMidnight = fireTimeFromMidnight
-        self.enabled = enabled
-        self.uuid = uuid
-    }
-    
+    //MARK: NSCoding
     func encode(with aCoder: NSCoder) {
         aCoder.encode(name, forKey: kName)
         aCoder.encode(fireTimeFromMidnight, forKey: kFireTimeFromMidnight)
         aCoder.encode(enabled, forKey: kEnabled)
         aCoder.encode(uuid, forKey: kUUID)
     }
-
+    
+    required init?(coder aDecoder: NSCoder) {
+    	guard let name = aDecoder.decodeObject(forKey: kName) as? String,
+            let uuid = aDecoder.decodeObject(forKey: kUUID) as? String else {return nil}
+        
+        self.name = name
+        self.uuid = uuid
+        self.fireTimeFromMidnight = aDecoder.decodeDouble(forKey: kFireTimeFromMidnight)
+        self.enabled = aDecoder.decodeBool(forKey: kEnabled)
+    }
 }
 
+
+func ==(lhs: Alarm, rhs: Alarm) -> Bool {
+    return lhs.uuid == rhs.uuid
+}
+
+/*
+Given an array that contains instances that conform to the MoFoEmployee protocol as described below:
+
+// Objective-C
+@protocol MoFoEmployee <NSObject>
+
+@property (strong, nonatomic) NSString *firstName;
+@property (strong, nonatomic) NSString *middleName;
+@property (strong, nonatomic) NSString *lastName;
+@property (strong, nonatomic) NSDate *startDate;
+
+@end
+
+// Swift
+public protocol MoFoEmployee: Comparable {
+    
+    var firstName: String { get set }
+    var middleName: String? { get set }
+    var lastName: String { get set }
+    var startDate: NSDate { get set }
+}
+
+write a method that prints the names of all employees that have started at least 30 days ago sorted in order of starting date.  Are there other ways to sort the array than the method you chose?  Describe any advantages or disadvantages of the approaches.
+
+*/
